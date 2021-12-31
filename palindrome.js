6d99a13b4a63882a5d783cbb2025921f3ffcb192
GOPIKAG <gopika568@gmail.com>
2021-12-31 17:35:05 +0530

    palindrome
    Signed-off-by: GOPIKAG <gopika568@gmail.com>

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,16 @@
 // Number palindrome
 function palindrome(num) {
-    var final = 0;
-    var temp = num;
-    while (num > 0) {
-      let a = num % 10;
-      num = parseInt(num / 10);
-      final = final * 10 + a;
-    }
-    if (final === temp) {
-      alert("This is a Palindrome");
-    } else alert("Not a Palindrome");
+  var final = 0;
+  var temp = num;
+  while (num > 0) {
+    let a = num % 10;
+    num = parseInt(num / 10);
+    final = final * 10 + a;
   }
+  if (final === temp) {
+    alert("This is a Palindrome");
+  } else alert("Not a Palindrome");
+}
 
 palindrome(123454321);
 
@@ -18,5 +18,5 @@ palindrome(123454321);
 let num = 123454321;
 let res = num.toString().split("").reverse().join();
 if (num === Number(res)) {
-        alert("This is a Palindrome");
-      } else alert("Not a Palindrome");
+  alert("This is a Palindrome");
+} else alert("Not a Palindrome");
